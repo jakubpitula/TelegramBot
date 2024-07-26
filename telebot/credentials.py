7535c7f7d6a3ce9f4dafbce27311b0aa7473b932
kuba <szachmistrz2@gmail.com>
2024-07-26 14:18:32 +0200

    url

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,2 +1,2 @@
 bot_user_name = "GoogleReviewTaskBot"
-URL = "https://app.circleci.com/pipelines/circleci/Sy2eLH2oYAzM2eAvEt7mCz/Q6qg7bNKSnYB4a6LrBswBg"
+URL = "https://jakubpitula.github.io/TelegramBot/"

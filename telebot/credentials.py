ef071acb6fe117a765e6f642b79b5526679c5b3b
kuba <szachmistrz2@gmail.com>
2024-07-26 13:45:47 +0200

    url

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,2 +1,2 @@
 bot_user_name = "GoogleReviewTaskBot"
-URL = "the heroku app link that we will create later"
+URL = "https://app.circleci.com/pipelines/circleci/Sy2eLH2oYAzM2eAvEt7mCz/Q6qg7bNKSnYB4a6LrBswBg"
